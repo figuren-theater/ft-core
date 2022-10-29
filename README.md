a5d3072c2d267b7ff7a5cd7dc6c3902e4ccf69c4
carstenbach <mail@carsten-bach.de>
2022-10-29 03:19:40 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ft-core
 Core components used from everywhere
+
+## PSR-4 autoloading 
+Use `namespace FigurenTheater\FtCore;` in `src/`
+
+Include the Composer autoloader with: 
+
+`require 'vendor/autoload.php';`
---
+++ ft-core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Ft Core
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ft-core
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ft_Core
+ */
+
+// Your code starts here.
