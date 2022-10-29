be9148d213bcdad64fc1597aa515b1eb43d02516
carstenbach <mail@carsten-bach.de>
2022-10-29 15:58:35 +0200

    Test

---
+++ src/Affe.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Figuren_Theater\Core;
+
+/**
+ * 		Affe
+ */
+class Affe
+{
+	
+	public function __construct()
+	{
+	}
+
+	public function laut()
+	{
+		return 'Hallo Affe';
+	}
+}
