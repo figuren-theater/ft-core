6c3b7c0cfd1c609f993c558cef3861363a756892
carstenbach <mail@carsten-bach.de>
2022-12-05 01:07:02 +0100

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # figuren.theater | Core
+
 Core components for a WordPress Multisite plattform like [figuren.theater](https://figuren.theater).
 
--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
  * Plugin Name:     figuren.theater | Core
  * Plugin URI:      https://github.com/figuren-theater/ft-core
  * Description:     Core components for a WordPress Multisite plattform like figuren.theater
- * Author:          Carsten Bach
- * Author URI:      https://figuren-theater
- * Text Domain:     ft-core
+ * Author:          figuren.theater
+ * Author URI:      https://figuren.theater
+ * Text Domain:     figurentheater
  * Domain Path:     /languages
- * Version:         1.0.13
+ * Version:         1.0.14
  *
  * @package         Figuren_Theater\Core
  */
