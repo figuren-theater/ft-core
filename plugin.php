5a6ea3da17693e7c87e84c4eca55122782a57784
carstingaxion <carstingaxion@users.noreply.github.com>
2023-07-14 15:54:09 +0000

    Update version to 1.2.17

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://figuren.theater
  * Text Domain:     figurentheater
  * Domain Path:     /languages
- * Version:         1.2.16
+ * Version:         1.2.17
  *
  * @package         Figuren_Theater
  */
