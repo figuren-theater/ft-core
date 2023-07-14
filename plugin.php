7b7d7f24c07dd1f142b2bdf14a1f90ca243c407b
Carsten Bach <mail@carsten-bach.de>
2023-07-14 04:53:13 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://figuren.theater
  * Text Domain:     figurentheater
  * Domain Path:     /languages
- * Version:         1.2.10
+ * Version:         1.2.12
  *
  * @package         Figuren_Theater
  */
